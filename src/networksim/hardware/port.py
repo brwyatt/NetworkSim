d57e5c63a4e96b2268709558832d21b80ce3b9a7
Bryan Wyatt <brwyatt@gmail.com>
2024-08-25 14:15:19 -0700

    Add Packet and Port queues

--- src/networksim/hardware/port.py
+++ src/networksim/hardware/port.py
@@ -1,15 +1,21 @@
 import logging
+from queue import Empty
+from queue import Full
+from queue import Queue
 from typing import Optional
 
 from networksim.hwid import HWID
+from networksim.packet import Packet
 
 
 logger = logging.getLogger(__name__)
 
 
 class Port:
-    def __init__(self, hwid: Optional[HWID] = None):
+    def __init__(self, hwid: Optional[HWID] = None, queue_length: int = 3):
         self.hwid = hwid
+        self.inbound_queue = Queue(queue_length)
+        self.outbound_queue = Queue(queue_length)
 
     @property
     def hwid(self) -> HWID:
@@ -25,3 +31,49 @@ class Port:
             )
 
         self._hwid = value
+
+    def outbound_write(self, packet: Packet):
+        try:
+            self.outbound_queue.put(packet, block=False)
+        except Full:
+            logger.warn(
+                "Failed to enqueue packet to Outbound Queue: Queue full!",
+            )
+
+    def outbound_read(self) -> Optional[Packet]:
+        try:
+            packet = self.outbound_queue.get(block=False)
+        except Empty:
+            logger.info(
+                "Failed to dequeue packet from Outbound Queue: Queue empty!",
+            )
+            return None
+
+        self.outbound_queue.task_done()
+        return packet
+
+    def inbound_write(self, packet: Packet):
+        try:
+            self.inbound_queue.put(packet, block=False)
+        except Full:
+            logger.warn(
+                "Failed to enqueue packet to Inbound Queue: Queue full!",
+            )
+
+    def inbound_read(self) -> Optional[Packet]:
+        try:
+            packet = self.inbound_queue.get(block=False)
+        except Empty:
+            logger.info(
+                "Failed to dequeue packet from Inbound Queue: Queue empty!",
+            )
+            return None
+
+        self.inbound_queue.task_done()
+        return packet
+
+    def send(self, packet: Packet):
+        self.outbound_write(packet)
+
+    def receive(self) -> Optional[Packet]:
+        return self.inbound_read()
---
+++ src/networksim/packet/__init__.py
@@ -0,0 +1,17 @@
+import logging
+
+from networksim.hwid import HWID
+
+
+logger = logging.getLogger(__name__)
+
+
+class Packet:
+    def __init__(self, src: HWID, dst: HWID, payload):
+        if type(src) is not HWID:
+            logger.error(f"src: expected `HWID` got `{type(HWID)}`")
+        self.src = src
+        if type(dst) is not HWID:
+            logger.error(f"dst: expected `HWID` got `{type(HWID)}`")
+        self.dst = dst
+        self.payload = payload
