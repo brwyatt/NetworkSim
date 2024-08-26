7b6c10787999ef9b82596a934f47274b8b059bf1
Bryan Wyatt <brwyatt@gmail.com>
2024-08-26 10:36:35 -0700

    Add/implement cable

---
+++ src/networksim/hardware/cable.py
@@ -0,0 +1,69 @@
+import logging
+
+from networksim.hardware.port import Port
+
+
+logger = logging.getLogger(__name__)
+
+
+class Cable:
+    def __init__(self, a: Port = None, b: Port = None, length: int = 3):
+        self.a = a
+        self.b = b
+        self.length = 3
+
+    @property
+    def a(self) -> Port:
+        return self._a
+
+    @a.setter
+    def a(self, port: Port):
+        if hasattr(self, "_b") and self._a:
+            self._a.disconnect()
+
+        if port is None:
+            self._a = None
+        elif port and type(port) is Port:
+            self._a = port
+            port.connect()
+
+        self.flush()
+
+    @property
+    def b(self) -> Port:
+        return self._b
+
+    @b.setter
+    def b(self, port: Port):
+        if hasattr(self, "_b") and self._b:
+            self._b.disconnect()
+
+        if port is None:
+            self._b = None
+        elif port and type(port) is Port:
+            self._b = port
+            port.connect()
+
+        self.flush()
+
+    def flush(self):
+        self.ab_transit = [None for x in range(0, 3)]
+        self.ba_transit = [None for x in range(0, 3)]
+
+    def step(self):
+        if self.a is None or self.b is None:
+            self.flush()
+            return
+
+        for x in range(0, self.length):
+            if x == 0:
+                if self.ab_transit[x] is not None:
+                    self.b.inbound_write(self.ab_transit[x])
+                if self.ba_transit[x] is not None:
+                    self.a.inbound_write(self.ba_transit[x])
+                continue
+            self.ab_transit[x - 1] = self.ab_transit[x]
+            self.ba_transit[x - 1] = self.ba_transit[x]
+
+        self.ab_transit[self.length - 1] = self.a.outbound_read()
+        self.ba_transit[self.length - 1] = self.b.outbound_read()
--- src/networksim/hardware/port.py
+++ src/networksim/hardware/port.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 class Port:
     def __init__(self, hwid: Optional[HWID] = None, queue_length: int = 3):
+        self.connected = False
         self.hwid = hwid
         self.inbound_queue = Queue(queue_length)
         self.outbound_queue = Queue(queue_length)
@@ -33,6 +34,9 @@ class Port:
         self._hwid = value
 
     def outbound_write(self, packet: Packet):
+        if not self.connected:
+            return
+
         try:
             self.outbound_queue.put(packet, block=False)
         except Full:
@@ -51,6 +55,13 @@ class Port:
 
         self.outbound_queue.task_done()
         return packet
+
+    def outbound_flush(self):
+        try:
+            while not self.outbound_queue.empty():
+                self.outbound_queue.get(block=False)
+        except Empty:
+            pass
 
     def inbound_write(self, packet: Packet):
         try:
@@ -72,8 +83,22 @@ class Port:
         self.inbound_queue.task_done()
         return packet
 
+    def inbound_flush(self):
+        try:
+            while not self.inbound_queue.empty():
+                self.inbound_queue.get(block=False)
+        except Empty:
+            pass
+
     def send(self, packet: Packet):
         self.outbound_write(packet)
 
     def receive(self) -> Optional[Packet]:
         return self.inbound_read()
+
+    def connect(self):
+        self.connected = True
+
+    def disconnect(self):
+        self.outbound_flush()
+        self.connected = False
