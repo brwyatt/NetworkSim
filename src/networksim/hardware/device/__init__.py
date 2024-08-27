19a300ed8523de446b23b3c35fd9e095b35b510c
Bryan Wyatt <brwyatt@gmail.com>
2024-08-26 19:44:59 -0700

    Add basic handling of device queues
    Just something to keep things "moving" and log the output

--- src/networksim/hardware/device/__init__.py
+++ src/networksim/hardware/device/__init__.py
@@ -1,9 +1,13 @@
+import logging
 from collections import defaultdict
 from typing import Optional
 
 from networksim.hardware.port import Port
 from networksim.helpers import randbytes
 from networksim.hwid import HWID
+
+
+logger = logging.getLogger(__name__)
 
 
 class Device:
@@ -21,8 +25,23 @@ class Device:
     def add_port(self, hwid: Optional[HWID] = None):
         self.ports.append(Port(hwid))
 
+    def process_payload(self, payload):
+        logger.info(payload)
+
     def step(self):
-        pass
+        for port in self.ports:
+            packet = port.receive()
+            if packet is not None:
+                if packet.dst not in [port.hwid, HWID.broadcast()]:
+                    logger.info(
+                        f"Ignoring packet from {packet.src} for {packet.dst} (not us!)"
+                    )
+                    continue
+                logger.info(
+                    f"Received {'broadcast' if packet.broadcast() else 'unicast'} "
+                    f"packet from {packet.src}"
+                )
+                self.process_payload(packet.payload)
 
     def __getitem__(self, index):
         return self.ports[index]
